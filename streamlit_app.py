e57c9880a19290a3a3a751690cc99f912052ebb7
rasikakurkute <143590216+rasikakurkute@users.noreply.github.com>
2024-02-14 11:00:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+ import streamlit
 import pandas
 import requests
 import snowflake.connector
@@ -55,23 +55,22 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
-# add a button to load the fruit
+#add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-    try:
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        my_data_rows = get_fruit_load_list(my_cnx)
-        streamlit.dataframe(my_data_rows)
-    except Exception as e:
-        streamlit.error("Error accessing Snowflake database. Please check your connection settings.")
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()   
+    streamlit.dataframe(my_data_rows)
 
-
+#allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding" +new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the list'):
-    try:
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        back_from_function = insert_row_snowflake(add_my_fruit, my_cnx)
-        streamlit.text(back_from_function)
-    except Exception as e:
-        streamlit.error("Error accessing Snowflake database. Please check your connection settings.")
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+streamlit.text(back_from_function)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 
